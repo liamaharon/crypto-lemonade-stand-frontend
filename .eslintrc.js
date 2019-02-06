86668892401c3a4ef6564aa28c6177f1fe1338e3
Liam Aharon <liam.aharon@hotmail.com>
2019-02-06 16:40:13 +1100

    Tweak eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "semi": [2, "always"]
+    "semi": [2, "always"],
+    "vue/require-default-prop": ["off"]
   },
   parserOptions: {
     parser: "babel-eslint"
