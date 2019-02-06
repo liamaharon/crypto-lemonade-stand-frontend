a77582f9497c31ecf2538ca4631f7449a48c21f6
Liam Aharon <liam.aharon@hotmail.com>
2019-02-07 10:44:51 +1100

    Move "all orders" view to its own component

---
+++ src/components/AllOrders.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h1 class="is-size-1">
+      All orders
+    </h1>
+    <article
+      v-if="ordersError"
+      class="message is-danger"
+    >
+      <div class="message-body">
+        There was a problem fetching the orders.
+        <br>
+        <span class="login-error-msg">
+          {{ ordersError }}
+        </span>
+      </div>
+    </article>
+    <div>
+      <div
+        v-for="order in ordersWithProduct"
+        :key="order.id"
+      >
+        <order 
+          class="order"
+          :ticker="order.product.ticker || 'Loading...'"
+          :qty="order.qty"
+          :account-id="order.accountId"
+          :status="order.status"
+          :payout-address="order.address"
+          :update-order="() => updateOrder(order.id)"
+          :updating="ordersUpdating[order.id] || false"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Order from '@/components/Order';
+
+export default {
+  name: "AllOrders",
+  components: {
+    Order
+  },
+  props: {
+    ordersWithProduct: Object,
+    ordersError: Object,
+    ordersUpdating: Object,
+    updateOrder: Function
+  }
+};
+</script>
+
+<style scoped>
+.order {
+  margin-bottom: 1rem;
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,47 +1,19 @@
 <template>
-  <div>
-    <h1 class="is-size-1">
-      All Orders
-    </h1>
-    <article
-      v-if="ordersError"
-      class="message is-danger"
-    >
-      <div class="message-body">
-        There was a problem fetching the orders.
-        <br>
-        <span class="login-error-msg">
-          {{ ordersError }}
-        </span>
-      </div>
-    </article>
-    <div>
-      <div
-        v-for="order in ordersWithProduct"
-        :key="order.id"
-      >
-        <order 
-          class="order"
-          :ticker="order.product.ticker || 'Loading...'"
-          :qty="order.qty"
-          :account-id="order.accountId"
-          :status="order.status"
-          :payout-address="order.address"
-          :update-order="() => updateOrder(order.id)"
-          :updating="ordersUpdating[order.id] || false"
-        />
-      </div>
-    </div>
-  </div>
+  <all-orders
+    :orders-with-product="ordersWithProduct"
+    :update-order="updateOrder"
+    :orders-error="ordersError"
+    :orders-updating="ordersUpdating"
+  />
 </template>
 
 <script>
-import Order from '../components/Order';
+import AllOrders from '@/components/AllOrders';
 
 export default {
   name: "Admin",
   components: {
-    Order
+    AllOrders
   },
   computed: {
     ordersWithProduct: function() {
